d09aaa4a3b7233aa6e2b0f7259084f712eb57b83
Ahmad Aziz <ahmadaziz0441@gmail.com>
2022-11-20 16:17:25 +0800

    added head section for music page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -28,7 +28,7 @@ export default function App(props: AppProps) {
     return (
         <>
             <Head>
-                <title>Page title</title>
+                <title>Ahmad Aziz Webpage</title>
                 <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width"/>
             </Head>
 
--- pages/music/index.tsx
+++ pages/music/index.tsx
@@ -5,6 +5,7 @@ import Aside from './Aside';
 import MainContent from "./MainContent";
 import {useMediaQuery} from "@mantine/hooks";
 import {useState} from 'react';
+import Head from "next/head";
 
 export default function Music() {
     const [opened, setOpened] = useState(false);
@@ -14,36 +15,43 @@ export default function Music() {
     const matches = useMediaQuery('(max-width: 765px)');
 
     return (
-        <AppShell
-            styles={{
-                main: {
-                    background: theme.colorScheme === 'dark' ? theme.colors.dark[8] : 'white',
-                },
-            }}
-            navbarOffsetBreakpoint="sm"
-            asideOffsetBreakpoint="sm"
-            navbar={
-                <Navbar opened={opened}/>
-            }
-            aside={
-                <Aside asideOpen={asideOpen}/>
-            }
-            /*footer={
-                <Footer height={60} p="md">
-                    Application footer
-                </Footer>
-            }*/
-            header={
-                matches ?
-                    <Header
-                        opened={opened}
-                        setAsideOpened={setAsideOpened}
-                        setOpened={setOpened}
-                    />
-                    : undefined
-            }
-        >
-            <MainContent/>
-        </AppShell>
+        <>
+            <Head>
+                <title>Music Player</title>
+                <meta name="title" content={'Music Player by Ahmad Aziz'}/>
+                <meta name="description" content={'Music Player by Ahmad Aziz'}/>
+            </Head>
+            <AppShell
+                styles={{
+                    main: {
+                        background: theme.colorScheme === 'dark' ? theme.colors.dark[8] : 'white',
+                    },
+                }}
+                navbarOffsetBreakpoint="sm"
+                asideOffsetBreakpoint="sm"
+                navbar={
+                    <Navbar opened={opened}/>
+                }
+                aside={
+                    <Aside asideOpen={asideOpen}/>
+                }
+                /*footer={
+                    <Footer height={60} p="md">
+                        Application footer
+                    </Footer>
+                }*/
+                header={
+                    matches ?
+                        <Header
+                            opened={opened}
+                            setAsideOpened={setAsideOpened}
+                            setOpened={setOpened}
+                        />
+                        : undefined
+                }
+            >
+                <MainContent/>
+            </AppShell>
+        </>
     );
 }
